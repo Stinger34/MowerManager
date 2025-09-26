fc544325ba24e1b7fce30e2c0a96574d33fe7671
Stinger34 <154110541+Stinger34@users.noreply.github.com>
2025-09-26 10:46:53 +0800

    Delete migrations-old directory

--- migrations-old/add-title-to-attachments.sql
+++
@@ -1,8 +0,0 @@
--- Migration to add title field to attachments table
--- This should be run against the database to add the new title column
-
-ALTER TABLE attachments 
-ADD COLUMN IF NOT EXISTS title TEXT;
-
--- Add a comment to document the purpose of the new column
-COMMENT ON COLUMN attachments.title IS 'User-provided title for the attachment, null if not provided';
